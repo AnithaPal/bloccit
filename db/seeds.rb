34101b40b435650298d860ba8fe2cae088473957
AnithaPal <anitha.aravindvel@gmail.com>
2015-06-10 09:09:10 -0400

    final commit in assignment 32

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,11 +19,16 @@ end
 
 uniq_post = Post.find_or_create_by!(title: "This is my unique title", body: "This is my unique body")
 uniq_comment = Comment.find_or_create_by!(post: uniq_post, body: "This is my unique comment")
+
+
 puts "Seed finished"
 puts "#{Post.count} posts are created"
 puts "#{Comment.count} comments are created"
-puts uniq_post
-puts uniq_comment
+
+puts uniq_post.title
+puts uniq_comment.body
+  
+
 
 require 'faker'
  
