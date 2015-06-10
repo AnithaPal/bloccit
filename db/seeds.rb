ddc24c4b36c34e7554ab32fabe9d807aeb0f9f1e
AnithaPal <anitha.aravindvel@gmail.com>
2015-06-10 09:04:46 -0400

    unique post and comments are added

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,9 +17,13 @@ posts = Post.all
 
 end  
 
+uniq_post = Post.find_or_create_by!(title: "This is my unique title", body: "This is my unique body")
+uniq_comment = Comment.find_or_create_by!(post: uniq_post, body: "This is my unique comment")
 puts "Seed finished"
 puts "#{Post.count} posts are created"
 puts "#{Comment.count} comments are created"
+puts uniq_post
+puts uniq_comment
 
 require 'faker'
  
