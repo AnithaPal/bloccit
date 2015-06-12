12d5d0c2fc233812b8938e6ca83e6fbe5539dbae
AnithaPal <anitha.aravindvel@gmail.com>
2015-06-12 07:00:01 -0400

    edit view is added and show view is modified with edit link

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,21 +23,23 @@ class PostsController < ApplicationController
     end  
   end  
 
+  
   def edit
-    @post = Post.find(params[:id])
+     @post = Post.find(params[:id])
+  end
+ 
+  def update
+     @post = Post.find(params[:id])
+     if @post.update_attributes(params.require(:post).permit(:title, :body))
+       flash[:notice] = "Post was updated."
+       redirect_to @post
+     else
+       flash[:error] = "There was an error saving the post. Please try again."
+       render :edit
+     end
   end
 
-  def update
-    @post Post.find(params[:id])
-    if @post.update_attributes(params.require(:post).permit(:title, :body))
-      flash[:notice] = "Post was updated"
-      redirect_to @post
-    else
-    flash[:error] = "There was an error saving the post. Please try again"
-    render :new
-    end  
-  end 
 
-  
+
 end
   
