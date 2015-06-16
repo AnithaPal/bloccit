909b9170a36c53848a0d4d66c121c01663ca7761
AnithaPal <anitha.aravindvel@gmail.com>
2015-06-16 06:05:50 -0400

    reordering scope is added

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
   default_scope { order('created_at DESC') }
+  scope :ordered_by_title, -> {reorder(title: :asc)}
+  scope :ordered_by_reverse_created_at, -> {reorder('created_at ASC')}
 end
