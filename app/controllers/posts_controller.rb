e7cd1f7b1e884ac13ef1956a66193044d65560c8
AnithaPal <anitha.aravindvel@gmail.com>
2015-06-17 09:14:13 -0400

    post controller is updated with topic id in each actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,12 +12,13 @@ class PostsController < ApplicationController
   end
 
   def new
+    @topic = Topic.find(params[:topic_id])
     @post = Post.new
     authorize @post
   end
 
   def create
-   
+    @topic = Topic.find(params[:topic_id])
     @post = current_user.posts.build(params.require(:post).permit(:title, :body))
     authorize @post
     if @post.save
@@ -31,11 +32,13 @@ class PostsController < ApplicationController
 
   
   def edit
+    @topic = Topic.find(params[:topic_id])
      @post = Post.find(params[:id])
      authorize @post
   end
  
   def update
+     @topic = Topic.find(params[:topic_id])
      @post = Post.find(params[:id])
      authorize @post
      if @post.update_attributes(params.require(:post).permit(:title, :body))
