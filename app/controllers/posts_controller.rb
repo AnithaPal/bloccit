bfb7d844e7cc1b0010670ed9ff851eb24ef4fd08
AnithaPal <anitha.aravindvel@gmail.com>
2015-06-17 10:47:12 -0400

    final commit in checkpoint 40

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,7 +23,7 @@ class PostsController < ApplicationController
     authorize @post
     if @post.save
       flash[:notice] = "Post was saved."
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the post. Please try again" 
       render :new 
@@ -43,7 +43,7 @@ class PostsController < ApplicationController
      authorize @post
      if @post.update_attributes(params.require(:post).permit(:title, :body))
        flash[:notice] = "Post was updated."
-       redirect_to @post
+       redirect_to [@topic, @post]
      else
        flash[:error] = "There was an error saving the post. Please try again."
        render :edit
