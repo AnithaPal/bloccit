ab6faf7f0230e35ec159a9e8ceaaeda85b84b537
AnithaPal <anitha.aravindvel@gmail.com>
2015-06-16 20:08:43 -0400

    post policy clas is updated with scope sub class

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,5 +1,21 @@
 class PostPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      if user.admin?
+        scope.all
+      elsif user.moderator?
+        scope.all
+      else
+        scope.where(:published => true)
+      end
+    end
+  end         
+
   def index?
     true
   end
+
+  def update?
+    user.admin? or not post.published?
+  end  
  end 
