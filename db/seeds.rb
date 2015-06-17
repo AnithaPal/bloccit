db89ab178b87f1673aa23a9298ff62b5c7978a7c
AnithaPal <anitha.aravindvel@gmail.com>
2015-06-16 22:29:03 -0400

    topics seed is added

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,16 @@ require 'faker'
  users = User.all
 
 
+ #create topics
+
+ 15.times do
+  Topic.create!(
+    name:  Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph)
+end
+topics = Topic.all
+
+
   # Note: by calling `User.new` instead of `create`,
  # we create an instance of User which isn't immediately saved to the database.
  
@@ -25,6 +35,7 @@ require 'faker'
 50.times do 
   Post.create!(
     user: users.sample,
+    topic: topics.sample,
     title: Faker::Lorem.sentence,
     body: Faker::Lorem.paragraph)
   
@@ -40,15 +51,44 @@ posts = Post.all
 
 end  
 
-user = User.first
- user.skip_reconfirmation!
- user.update_attributes!(
-   email: 'name@youremail.com',
-   password: 'helloworld'
- )
+#Create an admin user
+admin = User.new(
+  name: 'Admin User',
+  email: 'admin@example.com',
+  password: 'helloworld',
+  role: 'admin')
+
+admin.skip_confirmation!
+admin.save!
+
+#Create a moderator
+
+moderator = User.new(
+  name: 'Moderator',
+  email: 'moderator@example.com',
+  password: 'helloworld',
+  role: 'moderator')
+
+moderator.skip_confirmation!
+moderator.save!
+
+#Create a member
+member = User.new(
+  name: 'Member',
+  email: 'member@example.com',
+  password: 'helloworld',
+  role: 'member')
+
+  member.skip_confirmation!
+  member.save!
+
+
+
+
 
 puts "Seed finished"
 puts "#{User.count} users are created"
+puts "#{Topic.count} topics are created"
 puts "#{Post.count} posts are created"
 puts "#{Comment.count} comments are created"
 
