edbc9296f528d2a16f57d87addbebc5cf2fc5cd6
AnithaPal <anitha.aravindvel@gmail.com>
2015-06-17 20:49:40 -0400

    summary model and controllers are created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150617015913) do
+ActiveRecord::Schema.define(version: 20150618002752) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -48,6 +48,12 @@ ActiveRecord::Schema.define(version: 20150617015913) do
     t.boolean  "resolved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "summaries", force: :cascade do |t|
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "topics", force: :cascade do |t|
