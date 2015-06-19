b0fce142ffc67256bafc44734e45bc88f99046e4
AnithaPal <anitha.aravindvel@gmail.com>
2015-06-19 09:53:26 -0400

    bug fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,41 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def update
-    if curent_user.update_attributes(user_params)
+    if current_user.update_attributes(user_params)
       flash[:notice] = "User information updated"
       redirect_to edit_user_registration_path
     else
       flash[:error] = "Invalid user infomation" 
       redirect_to edit_user_registration_path
+    end  
   end
 
   private
 
   def user_params
     params.require(:user).permit(:name)
-  end  
-end    
+  end 
+end  
+
+
+
+# class UsersController < ApplicationController
+#    before_action :authenticate_user!
+ 
+#    def update
+#      if current_user.update_attributes(user_params)
+#        flash[:notice] = "User information updated"
+#        redirect_to edit_user_registration_path
+#      else
+#        flash[:error] = "Invalid user information"
+#        redirect_to edit_user_registration_path
+#      end
+#    end
+ 
+#    private
+ 
+#    def user_params
+#      params.require(:user).permit(:name)
+#    end
+#  end
