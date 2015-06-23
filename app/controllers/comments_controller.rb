8c9f2804bb7399251d998b488aef0b5b3f729570
AnithaPal <anitha.aravindvel@gmail.com>
2015-06-23 15:32:11 -0400

    completed cp 46

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,20 @@
 class CommentsController < ApplicationController
-  def new
-    @comment = Comment.new
-  end
+  
 
   def create
 
     @topic = Topic.find(params[:topic_id])
     @post = @topic.posts.find(params[:post_id])
-    @comment = @post.comments.build(comment_params)
-    @comment.user= @user
+    @comment = @post.comments.new(comment_params)
+    #@comment.post = @post
+    #@comment.user= current_user
+    @comment.user_id = current_user.id
     if @comment.save
       flash[:notice] = "Comment was saved"
       redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving a comment. Please try again"  
-      render :new
+      redirect_to [@topic, @post]
     end
   end
   
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,8 @@ class PostsController < ApplicationController
   def show
     @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
-    @comment = @post.comment
+    @comments = @post.comments
+    @comment = Comment.new
   end
 
   def new
