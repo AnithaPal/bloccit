6a2e0f7a00b36e52451c1bf0c2e476922dac3750
AnithaPal <anitha.aravindvel@gmail.com>
2015-06-23 11:43:23 -0400

    comment controller is updated with new and create actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,27 @@
 class CommentsController < ApplicationController
   def new
+    @comment = Comment.new
   end
+
+  def create
+
+    @topic = Topic.find(params[:topic_id])
+    @post = @topic.posts.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.user_id = @current_user.id
+    if @comment.save
+      flash[:notice] = "Comment was saved"
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "There was an error saving a comment. Please try again"  
+      render :new
+    end
+  end
+  
+  private 
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end   
+   
 end
