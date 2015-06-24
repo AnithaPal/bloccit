63ef2e75c22e65333f571fb4b6c43a82065a7827
AnithaPal <anitha.aravindvel@gmail.com>
2015-06-23 22:53:47 -0400

    post destroy action created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -53,6 +53,22 @@ class PostsController < ApplicationController
      end
   end
 
+  def destroy
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    authorize @post
+
+    if @post.destroy
+      flash[:notice] = "\"#{@post.title}\" was deleted successfully"
+      redirect_to @topic
+    else
+      flash[:error] = "There was an error deleting the post. Please try again."  
+      redirect_to topic_show_path
+    end
+  end    
+
+
+
   private
   def post_params
     params.require(:post).permit(:title, :body)
