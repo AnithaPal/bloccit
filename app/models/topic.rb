7d87d94486c20e7a1be543bf9b11024d40a5f00b
AnithaPal <anitha.aravindvel@gmail.com>
2015-06-27 12:15:55 -0400

    in progress

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,9 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
-  scope :visible_to, -> (user) {user ? all : where(public: true)}
+  #scope :public, -> {where(public: true)}
+  # scope :visible_to, -> (user) :publicly_viewable
+  scope :visible_to, -> {user ? all : where(public: true)}
+  scope :publicly_viewable, -> { user ? all : where(public: true) }
+  scope :privately_viewable, -> { user ? all : where(public: false) }
+
 end
