a41564ff2646fbc8b111eba969cc224832714df8
AnithaPal <anitha.aravindvel@gmail.com>
2015-06-28 13:12:15 -0400

    public profiles feature included

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -7,6 +7,10 @@ require 'rails_helper'
  
    before do
      @user = authenticated_user
+     @post = associated_post(user: @user)
+     @comment = Comment.new(user: @user, post: @post, body: "A comment")
+     allow(@comment).to receive(:send_favorite_emails)
+     @comment.save 
    end
  
    describe "not signed in" do
@@ -14,6 +18,9 @@ require 'rails_helper'
      it "shows profile" do
        visit user_path(@user)
        expect(current_path).to eq(user_path(@user))
+       expect(page).to have_content(@user.name)
+       expect(page).to have_content(@post.title)
+       expect(page).to have_content(@comment.body)
      end
  
    end
